c2dfb659ece5b667e2102320b137124d9d0a04ee
blurryfacebeat <blurryfacebeat@gmail.com>
2022-01-26 23:52:03 +0700

    Fix bug

--- src/shared/App.jsx
+++ src/shared/App.jsx
@@ -2,7 +2,7 @@ import { hot } from 'react-hot-loader/root';
 import React from 'react';
 
 const App = () => {
-  return <div>Hello qegqeqeg21</div>;
+  return <div>Hello qegqeqegwqwqg</div>;
 };
 
 export default hot(App);
